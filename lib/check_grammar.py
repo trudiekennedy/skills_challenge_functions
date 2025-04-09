185ed0b3d67d079cfe26a24e423bd11fb2942673
trudie anderson <trudiekennedy@gmail.com>
2025-04-09 12:35:15 +0100

    Updating code & tests for grammar_checker

---
+++ lib/check_grammar.py
@@ -0,0 +1,5 @@
+def check_grammar(text):
+    if text == "":
+        raise Exception("No text to analyse!")
+    else:
+        return text[0].isupper() and text[-1] in ["!", "?", "."]
---
+++ tests/test_check_grammar.py
@@ -0,0 +1,28 @@
+from lib.check_grammar import *
+import pytest
+
+def test_check_for_empty_string():
+    with pytest.raises(Exception) as e:
+        check_grammar("")
+    error_msg = str(e.value)
+    assert error_msg == "No text to analyse!"
+
+def test_check_first_letter_isnt_capitalised():
+    result = check_grammar("pandas are the best")
+    assert result == False
+
+def test_check_first_letter_isnt_capitalised_with_period():
+    result = check_grammar("pandas are the best.")
+    assert result == False    
+
+def test_check_first_letter_capitalised_with_period():
+    result = check_grammar("Pandas are the best.")
+    assert result == True
+
+def test_check_capitalised_without_punctuation_ender():
+    result = check_grammar("Pandas are the best")
+    assert result == False
+
+def test_check_capitalised_with_other_punctuation():
+    result = check_grammar("Pandas are best,")
+    assert result == False
